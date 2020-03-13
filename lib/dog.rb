fd2bbbddd9028ee3626fd85626fe220e454ffdc2
laarchambault <laarchambault@gmail.com>
2020-03-13 13:55:14 -0400

    progress

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
+require 'pry'
 class Dog
-    attr_accessor :name, :breed
-    attr_reader :id
+    attr_accessor :name, :breed, :id
 
     def initialize(name:, breed:, id:nil)
         @name = name
@@ -44,14 +44,53 @@ class Dog
             VALUES (?, ?)
         SQL
 
-        DB[:conn].execute(sql, self.name, self,breed)
+        DB[:conn].execute(sql, self.name, self.breed)
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
         end
-        #try using find_by_id to return instance of this class
+        self
     end
 
     def self.create(hash)
-        
+        dog = self.new(name: hash[:name], breed: hash[:breed])
+        hash.each do |key, value|
+            dog.send(("#{key}="), value)
+        end
+        dog.save
+        dog
+    end
+
+    def self.new_from_db(row)
+        self.new(name: row[1], breed: row[2], id: row[0])
+    end
+
+    def self.find_by_id(given_id)
+        sql = <<-SQL
+            SELECT * FROM dogs
+            WHERE id = ?
+        SQL
+
+        dog_row = DB[:conn].execute(sql, given_id)
+        dog = self.new_from_db(dog_row[0])
+        dog
+    end
+
+
+    def self.find_or_create_by(name:, breed:)
+        sql = <<-SQL
+        SELECT * FROM dogs
+        WHERE name = ? AND breed = ?
+        LIMIT 1
+        SQL
+
+        dog_arr = (DB[:conn].execute(sql, name, breed))[0]
+        #binding.pry
+        if !dog_arr.empty?
+            new_dog = self.find_by_id(dog_arr[0])
+        else
+            new_dog = self.new(name: dog_arr[1], breed: dog_arr[2])
+            new_dog.save
+        end
+        new_dog
     end
 
 end
